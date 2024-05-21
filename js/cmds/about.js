2adb6ad4690364a445509dbabcefc73875989942
RodolpheThienard <thienard.rodolphe1@gmail.com>
2024-05-21 21:08:29 +0200

    About me

---
+++ js/cmds/about.js
@@ -0,0 +1,12 @@
+export default function(output) {
+    output.innerHTML += `
+    <div style="display: grid; gap: 2rem; grid-template-columns: 1fr 3fr; align-items: center;">
+    <img src="../../images/Rodolphe_moustache.jpg" alt="Rodolphe" style="width:100%;" >
+        <p style="text-align:left; max-width: 700px;">
+        My name is Rodolphe, and I'm a student in high-performance computing and simulation at the Université Paris-Saclay in Paris. I played competitive volleyball in high school.
+        I spend most of my time working on personal projects. I'm also interested in Linux and free software. I enjoy code optimization and low-level programming. 
+        Apart from that, I'm a biker who likes to cook and hike.
+        </p>
+</div>
+`;
+}
