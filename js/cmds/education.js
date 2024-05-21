69af0d3bd2c6195c08f7bc0c449d9505c1ceab39
RodolpheThienard <thienard.rodolphe1@gmail.com>
2024-05-21 20:46:58 +0200

    Update Title format for degree

--- js/cmds/education.js
+++ js/cmds/education.js
@@ -3,7 +3,7 @@ export default function(output) {
     <br>Master High Performance Computation and Simulation</br>
     <span data-color="white">Paris-Saclay University</span> <span data-color="grey">|</span> <span data-color="white">2022 - 2024</span>
     
-    <p>Computer science degree</p>
+    <br>Computer science degree</br>
     <span data-color="white">UVSQ University</span> <span data-color="grey">|</span> <span data-color="white">2019 - 2022</span>
 
 `;
