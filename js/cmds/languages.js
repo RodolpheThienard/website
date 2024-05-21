45729b83902c41e0d169e2959f31c5fd70118976
RodolpheThienard <thienard.rodolphe1@gmail.com>
2024-05-21 20:38:04 +0200

    Languages command
    Use github-readme-stats to display the most used languages

---
+++ js/cmds/languages.js
@@ -0,0 +1,5 @@
+export default function(output) {
+    output.innerHTML += `\
+    <img src="https://github-readme-stats.vercel.app/api/top-langs/?username=rodolphethienard&langs_count=8&layout=compact&hide=gnuplot,TeX" alt="github language">
+    `}
+
