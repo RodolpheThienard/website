980777cef5b5ee0b2cab345591e7f4d46a142c40
RodolpheThienard <thienard.rodolphe1@gmail.com>
2024-05-21 20:44:50 +0200

    Education and experiences

---
+++ js/cmds/education.js
@@ -0,0 +1,12 @@
+export default function(output) {
+    output.innerHTML += `\
+    <br>Master High Performance Computation and Simulation</br>
+    <span data-color="white">Paris-Saclay University</span> <span data-color="grey">|</span> <span data-color="white">2022 - 2024</span>
+    
+    <p>Computer science degree</p>
+    <span data-color="white">UVSQ University</span> <span data-color="grey">|</span> <span data-color="white">2019 - 2022</span>
+
+`;
+}
+
+
---
+++ js/cmds/experiences.js
@@ -0,0 +1,24 @@
+export default function(output) {
+    output.innerHTML += `\
+    <br>- 1st place at Teratec Hackathon, Europe</br>
+    <span data-color="white">  HPC competition to port and optimize applications on AWS Graviton3E</span> <span data-color="grey">|</span> <span data-color="white">2024</span>
+    
+    <br>- Intership at EMG2</br>
+    <span data-color="white">  Technological validation of hardware integration of a POSIT GPGPU core on FPGA for HPC/HPDA and Deep learning applications</span> <span data-color="grey">|</span> <span data-color="white">2024</span>
+    
+    <br>- Intership at the French Atomic Energy and Alternative Energies Commission (CEA)</br>
+    <span data-color="white">  Designing a user-space thread scheduler for MPC</span> <span data-color="grey">|</span> <span data-color="white">2023</span>
+    
+    <br>- Programming project, Paris Saclay</br>
+    <span data-color="white">  Validation of Modular Assembly Quality Analyzer and Optimizer (MAQAO ) metrics</span> <span data-color="grey">|</span> <span data-color="white">2022</span>
+
+    <br>- Intership at David Laboritory, UVSQ </br>
+    <span data-color="white">  Development of a prototype as part of the cod-prof project with INSEE</span> <span data-color="grey">|</span> <span data-color="white">2022</span>
+    
+    <br>- Freelance Website D&H Paysages</br>
+    <span data-color="white">  Creation of a website for the landscaping company</span> <span data-color="grey">|</span> <span data-color="white">2021</span>
+
+`;
+}
+
+
