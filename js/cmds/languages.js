5d0ebcfffc4f36176677515d7ae4edffc7974d6f
RodolpheThienard <thienard.rodolphe1@gmail.com>
2024-05-23 11:33:36 +0200

    Remove Lua language in language commands

--- js/cmds/languages.js
+++ js/cmds/languages.js
@@ -1,5 +1,6 @@
 export default function(output) {
     output.innerHTML += `\
-    <img src="https://github-readme-stats.vercel.app/api/top-langs/?username=rodolphethienard&langs_count=8&layout=compact&hide=gnuplot,TeX" alt="github language">
+    <img src="https://github-readme-stats.vercel.app/api/top-langs/?username=rodolphethienard&langs_count=8&layout=compact&hide=gnuplot,TeX,Lua" alt="github language">
+    
     `}
 
